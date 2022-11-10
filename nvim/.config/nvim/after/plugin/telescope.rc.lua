b197a64b6f14bd241b99254f11feed26e00f076c
fraso-dev <fraso.dev@protonmail.com>
2022-11-10 16:56:00 +0100

    nvim: telescope multi-selection functions

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -1,20 +1,59 @@
 local status, telescope = pcall(require, "telescope")
 if (not status) then return end
 local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 local builtin = require("telescope.builtin")
 
 local function telescope_buffer_dir()
   return vim.fn.expand('%:p:h')
 end
 
-local fb_actions = require'telescope'.extensions.file_browser.actions
+local fb_actions = require 'telescope'.extensions.file_browser.actions
+
+local telescope_custom_actions = {}
+
+function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selected_entry = action_state.get_selected_entry()
+  local num_selections = #picker:get_multi_selection()
+  if not num_selections or num_selections <= 1 then
+    actions.add_selection(prompt_bufnr)
+  end
+  actions.send_selected_to_qflist(prompt_bufnr)
+  vim.cmd("cfdo " .. open_cmd)
+end
+
+function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "vsplit")
+end
+
+function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "split")
+end
+
+function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "tabe")
+end
+
+function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+end
 
 telescope.setup {
   defaults = {
     mappings = {
-      n = {
-        ["q"] = actions.close
+      i = {
+        ["<ESC>"] = actions.close,
+        ["<C-J>"] = actions.move_selection_next,
+        ["<C-K>"] = actions.move_selection_previous,
+        ["<S-TAB>"] = actions.toggle_selection + actions.move_selection_next,
+        ["<TAB>"] = actions.toggle_selection + actions.move_selection_previous,
+        ["<CR>"] = telescope_custom_actions.multi_selection_open,
+        ["<C-V>"] = telescope_custom_actions.multi_selection_open_vsplit,
+        ["<C-S>"] = telescope_custom_actions.multi_selection_open_split,
+        ["<C-T>"] = telescope_custom_actions.multi_selection_open_tab,
       },
+      n = i,
     },
   },
   extensions = {
@@ -50,6 +89,9 @@ vim.keymap.set('n', ';f',
     })
   end)
 vim.keymap.set('n', ';r', function()
+  builtin.registers()
+end)
+vim.keymap.set('n', ';a', function()
   builtin.live_grep()
 end)
 vim.keymap.set('n', ';b', function()
